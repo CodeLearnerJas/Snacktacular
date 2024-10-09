a82a9a1a0728ac7ec5a8cb37bdc2cffd313f3d0b
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-10-10 09:44:25 +1100

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by GuitarLearnerJas on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnacktacularTests/SnacktacularTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularTests.swift
+//  SnacktacularTests
+//
+//  Created by GuitarLearnerJas on 10/10/2024.
+//
+
+import Testing
+@testable import Snacktacular
+
+struct SnacktacularTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
